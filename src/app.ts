712f68bea7fdc206fdbd194f28681da771467c27
jarkurghan <jarkurghan@gmail.com>
2026-03-12 10:37:19 +0000

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import routes from "./routes";
 
 const app = new Hono();
 
+app.use('*', logger())
 app.route("/api", routes);
 
 export default app;
