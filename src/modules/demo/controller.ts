ff3f46919a18f4e2c0b2a25e5440455af49998a6
jarkurghan <jarkurghan@gmail.com>
2026-04-26 05:19:41 +0100

    remove demo code

--- src/modules/demo/controller.ts
+++
@@ -1,5 +0,0 @@
-import type { Context } from "hono";
-
-export const getDemo = async (c: Context) => {
-    return c.json({ message: "Hello World" });
-};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Hono } from "hono";
 import insta from "./insta";
 import anime from "./anime";
 import movie from "./movie";
-import demo from "./demo";
 import pray from "./pray";
 
 const routes = new Hono();
@@ -11,6 +10,5 @@ routes.route("/prayer-time", pray);
 routes.route("/insta-saver", insta);
 routes.route("/anime", anime);
 routes.route("/movie", movie);
-routes.route("/demo", demo);
 
 export default routes;
